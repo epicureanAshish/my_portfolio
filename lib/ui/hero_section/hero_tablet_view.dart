e3513b443717a01326441255d886e9f1fafea173
ashishchauhan <ashishchauhan0206@gmail.com>
2026-05-31 13:47:32 +0530

    Update

--- lib/ui/hero_section/hero_tablet_view.dart
+++ lib/ui/hero_section/hero_tablet_view.dart
@@ -13,7 +13,7 @@ class HeroTabletView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
